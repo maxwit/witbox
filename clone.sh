f2949fed1816d45205783ee9fe5244b8ab33191e
Sandy Zhou <sandy.zhou@maxwit.com>
2013-05-13 15:13:45 +0800

    add clone.sh
    Signed-off-by: Sandy Zhou <sandy.zhou@maxwit.com>

---
+++ clone.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+TOP_DIR="/maxwit"
+SERVER="192.168.0.2"
+GITADMIN="gitolite-admin"
+
+check_out()
+{
+	cd $TOP_DIR
+
+	if [ -d $1 ]; then
+		cd $1 && git pull
+	else
+		git clone git@${SERVER}:$1.git $1
+	fi
+}
+
+check_out ${GITADMIN}
+echo
+
+DIRS=`grep repo ${GITADMIN}/conf/gitolite.conf | sed -e 's/repo //' -e 's/gitolite-admin//'`
+
+for dir in $DIRS
+do
+	check_out $dir
+	echo
+done
