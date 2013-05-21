b909edbe3c8a335a8a01550cdc7d888a263baf38
Emily Qin <emily.qin@maxwit.com>
2013-05-21 14:47:33 +0800

    add git server setup
    Signed-off-by: Emily Qin <emily.qin@maxwit.com>

--- server/setup.sh
+++ server/setup.sh
@@ -2,6 +2,32 @@
 
 git_setup()
 {
+	sudo yum install git git-daemon
+
+	path="$HOME/.ssh"
+
+	git config --list | grep ^color.ui || \
+	git config --global color.ui auto
+
+	if [ ! -x "$path" ]; then
+			exit 1
+	else
+		sudo sed -i '/\:OUTPUT ACCEPT \[0\:0\]/a\-I INPUT -p tcp --dport 9418 -j ACCEPT' /etc/sysconfig/iptables
+		sudo /etc/init.d/iptables restart
+		sudo sed -i '$a\git daemon --reuseaddr --base-path=/server/git/repositories &' /etc/rc.local
+
+		sudo useradd -d /server/git git
+		echo "please input passwd to set git passwd"
+		sudo passwd git
+		cp $path/*.pub /tmp
+
+		cd /tmp
+		git clone git://github.com/sitaramc/gitolite
+		echo "please input git passwd"
+		su git -c 'mkdir -p /server/git/bin'
+		su git -c '/tmp/gitolite/install -to /server/git/bin'
+		su git -c '/server/git/bin/gitolite setup -pk /tmp/*.pub'
+	fi
 	return
 }
 
