54183d4171db76d8738c72d7f8dff86b4ba36400
Conke Hu <conke.hu@maxwit.com>
2013-05-21 14:34:52 +0800

    fix init
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- server/setup.sh
+++ server/setup.sh
@@ -2,33 +2,37 @@
 
 git_setup()
 {
-
+	return
 }
 
 cups_setup()
 {
+	HPLIP_VER="3.13.5"
 
+	if [ ! -e /maxwit/soruce/hplip-${HPLIB_VER}.tar.gz ]; then
+		wget -c -P /maxwit/soruce http://prdownloads.sourceforge.net/hplip/hplip-${HPLIB_VER}.tar.gz
+	fi
 }
 
 samba_setup()
 {
-
+	return
 }
 
 mysql_setup()
 {
-
+	return
 }
 
 apache_setup()
 {
-
+	return
 }
 
 
 nfs_setup()
 {
-
+	return
 }
 
 git_setup
