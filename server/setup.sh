2ac3da34e227a3761680805148b36eeeeab855c8
Conke Hu <conke.hu@maxwit.com>
2013-05-21 16:27:52 +0800

    HP driver installation
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- server/setup.sh
+++ server/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+TOP_DIR=$PWD
+BUILD_PATH="/maxwit/build"
+SOURCE_PATH="/maxwit/source"
+
+WGET="wget -c -P $SOURCE_PATH"
+
 git_setup()
 {
 	return
@@ -9,9 +15,28 @@ cups_setup()
 {
 	HPLIP_VER="3.13.5"
 
-	if [ ! -e /maxwit/soruce/hplip-${HPLIB_VER}.tar.gz ]; then
-		wget -c -P /maxwit/soruce http://prdownloads.sourceforge.net/hplip/hplip-${HPLIB_VER}.tar.gz
+	if [ ! -e $SOURCE_PATH/hplip-${HPLIP_VER}.tar.gz ]; then
+		$WGET http://ncu.dl.sourceforge.net/project/hplip/hplip/3.13.5/hplip-${HPLIP_VER}.tar.gz
 	fi
+
+	if [ ! -e $SOURCE_PATH/hplip-${HPLIP_VER}-plugin.run ]; then
+		$WGET http://www.openprinting.org/download/printdriver/auxfiles/HP/plugins/hplip-${HPLIP_VER}-plugin.run
+	fi
+
+	if [ ! -e $BUILD_PATH/.powertool_built ]; then
+		cd $BUILD_PATH
+		tar xvf $SOURCE_PATH/hplip-${HPLIP_VER}.tar.gz && \
+		cd hplip-${HPLIP_VER} && \
+		./configure --prefix=/usr && \
+		make && \
+		sudo make install && touch $BUILD_PATH/.powertool_built || exit 1
+	fi
+
+	cd $BUILD_PATH
+	cp -v $SOURCE_PATH/hplip-${HPLIP_VER}-plugin.run . && \
+	patch -p0 < $SOURCE_PATH/hplip-${HPLIP_VER}-plugin.patch && \
+	chmod +x hplip-${HPLIP_VER}-plugin.run && \
+	sudo ./hplip-${HPLIP_VER}-plugin.run --nox11 < $TOP_DIR/plugin || exit 1
 }
 
 samba_setup()
