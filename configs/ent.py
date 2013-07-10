7c5841f8b2510d4118bf11f412ab10708e11a3ab
Conke Hu <conke.hu@maxwit.com>
2013-07-10 20:06:38 +0800

    misc fix
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- configs/ent.py
+++ configs/ent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import platform
 
 def do_setup(distrib, version, config):
 	user = config['user.name']
@@ -71,45 +72,79 @@ def do_setup(distrib, version, config):
 
 msmtp_config = {}
 
-def do_report(key, rep_fn, config_list):
-	print 'Getting report information...'
-	rep_fd = open(rep_fn, "aw")
-	if key == 'install':
-		rep_fd.write('Installation setting report:\n\n')
-		rep_fd.flush()
-		os.system("dpkg -l vim vim-gnome gcc g++ >> %s" % rep_fn)
-		rep_fd.write('\n')
-	elif key == 'mail':
-		rep_fd.write('Mail setting report:\n\n')
-		rep_fd.flush()
-		os.system("dpkg -l msmtp mutt >> %s" % rep_fn)
-		rep_fd.write('\n')
-		user = os.getenv('USER')
-		fd = open('/home/%s/.msmtprc' % user)
-		for line in fd:
-			if line != '\n' and line.strip().startswith('#') == False:
-				key_value = line.strip().split(' ', 1)
-				if len(key_value) == 2:
-					msmtp_config[key_value[0].strip()] = key_value[1].strip()
+def do_report(task, fd, config_list):
+	if task == 'help':
+		print 'usage:\n' \
+			'./powertool -r install: Report System Installation\n' \
+			'./powertool -r command: Report Linux Commands Practice\n'
 		fd.close()
+		exit()
 
-		if msmtp_config.has_key('host') != True:
-			rep_fd.write('Host is not configured in file msmtprc!\n')
-		elif msmtp_config['host'] != 'smtp.qq.com':
-			rep_fd.write('Expected host is smtp.qq.com, yours is %s\n' % msmtp_config['host'])
-		else:
-			rep_fd.write('Host is configured correctly!\n')
+	print 'checking %s ...' % task
+	count = 1
 
-		if msmtp_config.has_key('user') != True:
-			rep_fd.write('User is not configured in file msmtprc\n')
-		elif msmtp_config['user'] != config_list['user.mail']:
-			rep_fd.write('Expected user is %s, yours is %s\n' % (config_list['user.mail'], msmtp_config['user']))
-		else:
-			rep_fd.write('User is configured correctly!\n')
+	### check install ###
+	if task == 'install':
+		fd.write("*")
+	fd.write("Step %d: Installation\n" % (count))
+	count += 1
 
-		if msmtp_config.has_key('from') != True:
-			rep_fd.write('From is not configured in file msmtprc\n')
-		elif msmtp_config['from'] != config_list['user.mail']:
-			rep_fd.write('Expected from is %s, yours is %s\n' % (config_list['user.mail'], msmtp_config['from']))
-		else:
-			rep_fd.write('From is configured correctly!\n')
+	fd.write('[Partition and File System Information]:\n')
+	fd_chk = open('/proc/mounts')
+	for line in fd_chk:
+		fd.write(line)
+	fd.write('\n')
+
+	fd.write('[Packages Installation]:\n')
+	for line in os.popen("dpkg -l vim vim-gnome gcc g++ msmtp mutt"):
+		fd.write(line)
+	fd.write('\n')
+
+	### check commands ###
+	if task == 'command':
+		fd.write("*")
+	fd.write("Step %d: Unix/Linux Commands\n" % (count))
+	count += 1
+
+	fd_hist = open(os.getenv('HOME') + '/.bash_history')
+	lc = 0
+	for line in fd_hist:
+		lc += 1
+		if lc > 1900:
+			fd.write("(%d): %s" % (lc - 1900, line))
+	fd_hist.close()
+
+	#fd.write('Mail setting report:\n\n')
+	#fd.flush()
+	#os.system("dpkg -l msmtp mutt >> %s" % fn)
+	#fd.write('\n')
+	#fd = open(os.getenv('HOME') + '/.msmtprc')
+	#for line in fd:
+	#	if line != '\n' and line.strip().startswith('#') == False:
+	#		task_value = line.strip().split(' ', 1)
+	#		if len(task_value) == 2:
+	#			msmtp_config[task_value[0].strip()] = task_value[1].strip()
+	#fd.close()
+
+	#if msmtp_config.has_task('host') != True:
+	#	fd.write('Host is not configured in file msmtprc!\n')
+	#elif msmtp_config['host'] != 'smtp.qq.com':
+	#	fd.write('Expected host is smtp.qq.com, yours is %s\n' % msmtp_config['host'])
+	#else:
+	#	fd.write('Host is configured correctly!\n')
+
+	#if msmtp_config.has_task('user') != True:
+	#	fd.write('User is not configured in file msmtprc\n')
+	#elif msmtp_config['user'] != config_list['user.mail']:
+	#	fd.write('Expected user is %s, yours is %s\n' % (config_list['user.mail'], msmtp_config['user']))
+	#else:
+	#	fd.write('User is configured correctly!\n')
+
+	#if msmtp_config.has_task('from') != True:
+	#	fd.write('From is not configured in file msmtprc\n')
+	#elif msmtp_config['from'] != config_list['user.mail']:
+	#	fd.write('Expected from is %s, yours is %s\n' % (config_list['user.mail'], msmtp_config['from']))
+	#else:
+	#	fd.write('From is configured correctly!\n')
+
+	fd.write('\n')
