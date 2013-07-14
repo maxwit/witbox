f50d689fab20017e3e9a6a5775fa9c6ae7b7ee68
Sandy Zhou <sandy.zhou@maxwit.com>
2013-07-14 17:47:15 +0800

    tex split enhancement
    Signed-off-by: Sandy Zhou <sandy.zhou@maxwit.com>

--- tex_split/split.py
+++ tex_split/split.py
@@ -11,6 +11,8 @@ def split(path):
 	basename = os.path.basename(path)
 	dirname = os.path.dirname(path)
 
+	fd_dst = open(dirname + '/split_' + basename, 'w+')
+
 	fd_begin = open('article_begin.tmpl')
 	str_begin = fd_begin.read()
 	fd_begin.close()
@@ -19,37 +21,48 @@ def split(path):
 	str_end = fd_end.read()
 	fd_end.close()
 
-	fd_ch = None
-	ch = 0
+	ch_fd = None
+	ch_no = 0
+
 	for line in fd_src:
 		match = re.search(r'^\\chapter{(.*)}', line)
 		if match:
-			if ch > 0:
-				fd_ch.write(str_end)
-				fd_ch.close()
+			if ch_fd <> None:
+				ch_fd.write(str_end)
+				ch_fd.close()
 
-			ch += 1
-			fd_ch = open(dirname + '/chapter%d.tex' % ch, 'w+')
+			ch_no += 1
+			ch_fn = 'chapter%d.tex' % ch_no
+			print ch_fn
+
+			ch_fd = open(dirname + '/' + ch_fn, 'w+')
 			chapter = match.groups()[0]
-			fd_ch.write(str_begin.replace('_TITLE_', chapter))
+			ch_fd.write(str_begin.replace('_TITLE_', chapter))
 
+			###
+			fd_dst.write(line)
+			fd_dst.write('\\input{%s}\n\n' % ch_fn)
 		else:
 			if re.match(r'^\s*\\end{document}\s*\n', line) <> None:
+				fd_dst.write(line)
 				break
 
-			if fd_ch <> None:
-				fd_ch.write(line)
+			if ch_fd <> None:
+				ch_fd.write(line)
+			else:
+				fd_dst.write(line)
 
-	if fd_ch <> None:
-		fd_ch.write(str_end)
-		fd_ch.close()
+	if ch_fd <> None:
+		ch_fd.write(str_end)
+		ch_fd.close()
 
 	fd_src.close()
+	fd_dst.close()
 
 if __name__ == "__main__":
 	parser = OptionParser()
 	parser.add_option('-f', '--file', dest='path',
-					  default=False, action='store_true',
+					  default=True, action='store_true',
 					  help="path to the file to be splitted")
 
 	(options, args) = parser.parse_args()
