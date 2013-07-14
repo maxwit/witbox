5bfded936f4cb8bcf6c327e482047de5fdceb153
Conke Hu <conke.hu@maxwit.com>
2013-07-14 15:45:01 +0800

    add parser
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- tex_split/split.py
+++ tex_split/split.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python
 
-fd_src = open('cpio_vol3.tex')
-fd_dst = open('cpio_vol3_split.tex', 'w+')
+import os,sys,re
+from optparse import OptionParser
 
-for line in fd_src:
-	fd_dst.write(line)
+def split(path):
+	print 'splitting %s ...' % path
 
-fd_dst.close()
-fd_src.close()
+	fd_src = open(path)
+	fd_dst = open(path + '_split.tex', 'w+')
+
+	for line in fd_src:
+		fd_dst.write(line)
+
+	fd_dst.close()
+	fd_src.close()
+
+if __name__ == "__split__":
+	parser = OptionParser()
+	parser.add_option('-f', '--file', dest='file',
+					  default=False, action='store_true',
+					  help="file to be splitted")
+
+	(options, args) = parser.parse_args()
+
+	if options.file:
+		split(args[0])
+		exit()
+
+	print 'usage: ...!'
