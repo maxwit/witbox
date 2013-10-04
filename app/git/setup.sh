d0629eac8ed1535a90d8e67a84ac899acf338bda
Conke Hu <conke.hu@maxwit.com>
2013-10-05 02:18:23 +0800

    add setup fuctions
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- app/git/setup.sh
+++ app/git/setup.sh
@@ -5,8 +5,7 @@ echo "---- GIT Configuration ---"
 git config --list | grep ^color.ui || \
 	git config --global color.ui auto
 
-git config --list | grep ^user.name || \
-	git config --global user.name "$1"
+git config --global user.name "$1"
 
 git config --list | grep ^user.email || \
 	git config --global user.email $2
