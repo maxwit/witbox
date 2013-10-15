a07741b33ffcf8f022a9a921a831f783ad442023
Conke Hu <conke.hu@maxwit.com>
2013-10-15 21:50:59 +0800

    add sshkey.sh
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

---
+++ sshkey.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+KEY_FILE="/tmp/${USER}.pub"
+
+cp ~/.ssh/id_rsa.pub ${KEY_FILE}
+echo "This is my ssh key." | mutt devel@maxwit.com -s "[SSH] ${USER}'s key" -a ${KEY_FILE} 
+
+rm ${KEY_FILE}
