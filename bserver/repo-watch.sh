d1421d9be282a75010f5ca58c6eb36e50c10c615
Sandy Zhou <sandy.zhou@maxwit.com>
2014-04-13 18:33:03 +0800

    update inode
    Signed-off-by: Sandy Zhou <sandy.zhou@maxwit.com>

--- bserver/repo-watch.sh
+++ bserver/repo-watch.sh
@@ -4,16 +4,13 @@ DOC_ROOT=$HOME/document
 
 tab=`mktemp`
 
-#for doc in `ls $DOC_ROOT`
-for doc in testing
+for doc in `ls $DOC_ROOT`
 do
-	if [ -d $DOC_ROOT/$doc ]; then
-		inode="/home/git/repositories/$doc.git/refs/heads"
-		if [ -e $inode ]; then
-			echo $inode IN_CLOSE_WRITE /home/conke/project/witbox/bserver/repo-build.sh \$@ >> $tab
-		else
-			echo "$inode: No such file"
-		fi
+	inode="/home/git/repositories/document/$doc.git/refs/heads"
+	if [ -e $inode ]; then
+		echo $inode IN_CLOSE_WRITE /home/conke/project/witbox/bserver/repo-build.sh \$@ >> $tab
+	else
+		echo "$inode: No such file"
 	fi
 done
 
