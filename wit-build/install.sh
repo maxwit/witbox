443aa21ebbad7502190f1d2298a8f9b3dcec6e10
Conke Hu <conke.hu@maxwit.com>
2014-05-07 23:46:19 -0700

    add wit-build
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

---
+++ wit-build/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ETC="/etc/wit"
+
+mkdir -p $ETC && \
+cp -v wit-watch $ETC && \
+cp -v wit-build /usr/bin/ || exit 1
+
+if [ -e /etc/rc.local ]; then
+	grep wit-watch /etc/rc.local || \
+		sed -i "/^exit/i $ETC/wit-watch\n" /etc/rc.local || exit 1
+else
+	echo "Warning: /etc/rc.local does not exist!"
+	exit 1
+fi
+
+echo "WitBuild installed successfully, enjoy!"
+echo
