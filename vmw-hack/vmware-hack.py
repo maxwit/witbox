039eccd4b525913ed791cd96ea44d3e98c396dab
Emily <emily.qin@maxwit.com>
2014-06-03 20:08:17 +0800

    add unlock_osx()
    Signed-off-by: Emily <emily.qin@maxwit.com>

--- vmw-hack/vmware-hack.py
+++ vmw-hack/vmware-hack.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
 
 import os
+import zipfile
 
 def vmx_debug(arg):
 	return os.system('sudo /usr/lib/vmware/bin/vmware-vmx-debug ' + arg)
 
 def unlock_osx():
-	pass
+	f = zipfile.ZipFile('unlock-all-v120.zip')
+	for file in f.namelist():
+		f.extract(file, '/tmp/')
+		mod = f.getinfo(file).external_attr >> 16 & 0x1ff
+		os.chmod('/tmp/' + file, mod)
+
+	os.chdir('/tmp/unlock-all-v120/linux')
+	os.system('sudo ./install.sh')
+	f.close()
 
 vmx_debug('-version')
 
