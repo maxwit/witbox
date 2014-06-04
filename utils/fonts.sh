33471d5e1e9065a694f5690ae947a84e01768dd0
Conke Hu <conke.hu@maxwit.com>
2014-06-03 22:37:26 -0700

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- utils/fonts.sh
+++ utils/fonts.sh
@@ -4,7 +4,7 @@ TMP_DIR=`mktemp -d`
 SERVER=192.168.1.1
 #SERVER=conke.oicp.net
 
-wget -r -l 1 -A ttf,ttc,TTF,TTC -P $TMP_DIR --cut-dirs=9 http://$SERVER/pub/utility/fonts/ && \
+wget -r -l 1 -A ttf,ttc,TTF,TTC -P $TMP_DIR --cut-dirs=9 http://$SERVER/pub/utils/fonts/ && \
 sudo mkdir -vp /usr/share/fonts/truetype/ && \
 sudo cp -v $TMP_DIR/$SERVER/* /usr/share/fonts/truetype/ && \
 rm -rf $TMP_DIR && \
