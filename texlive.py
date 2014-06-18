161e74d928956aaa9655ee92dabf1d1b85017500
Conke Hu <conke.hu@maxwit.com>
2014-06-18 16:43:46 -0700

    init add texlive.py
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

---
+++ texlive.py
@@ -0,0 +1,5 @@
+http://mirrors.zju.edu.cn/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz
+http://mirrors.zju.edu.cn/CTAN/systems/texlive/tlnet/install-tl.zip
+
+sudo install-tl --gui --location http://oss.ustc.edu.cn/CTAN/systems/texlive/tlnet/
+sudo install-tl --gui --location http://ftp.ctex.org/mirrors/texlive/tlnet/
