6b00c9e0fea1ef20fbe7020d6dd6b3d369ab64ff
Conke Hu <conke.hu@maxwit.com>
2015-02-14 03:13:58 -0800

    update grub.sh
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- witdisk/grub.sh
+++ witdisk/grub.sh
@@ -8,33 +8,86 @@ fi
 
 boot=${1%/}
 
-disk=""
+part=""
+mp=""
 while read mnt
 do
 	mnt=($mnt)
 	mp=${mnt[1]}
-	if [ ${boot:0:${#mp}} == $mp ]
+	#if [ ${boot:0:${#mp}} == $mp ]
+	if [ ${boot} == $mp ]
 	then
-		disk=${mnt[0]}
+		part=${mnt[0]}
 		break
 	fi
 done < /proc/mounts
 
-if [ "$disk" == "" ]
+if [ "$part" == "" ]
 then
 	echo "No such mount point found! ($boot)"
 	exit 1
 fi
 
-disk=${disk%%[0-9]}
+disk=${part%%[0-9]}
 
+############# install grub #############
 echo "installing grub to $boot for $disk ..."
-exit 1
 
-grub=`which grub2-install`
-if [ -z $grub ]
+grub_cmd=`which grub2-install`
+if [ -z $grub_cmd ]
 then
-	grub="grub-install"
+	grub_cmd="grub-install"
 fi
 
-$grub --boot-directory=$boot $disk
+$grub_cmd --boot-directory=$boot $disk
+
+############# generate grub.cfg #############
+if [ -d "$mp/grub2" ]
+then
+	grub_cfg="$mp/grub2/grub.cfg"
+elif [ -d "$mp/grub" ]
+then
+	grub_cfg="$mp/grub/grub.cfg"
+else
+	echo "The grub directory does not exist!"
+	exit 1
+fi
+
+echo "Generating $grub_cfg ..."
+echo "GRUB_TIMEOUT=5" > $grub_cfg
+
+for iso in `ls $mp/iso/*.iso`
+do
+	fn=`basename $iso`
+	# FIXME!
+	dist=(${fn//-/ })
+
+	id=${dist[0]}
+	ver=${dist[1]}
+
+	echo "generating menuentry for $id-$ver ..."
+	case $id in
+	CentOS|RHEL|Fedora|OLinux)
+		uuid=`blkid $part | sed 's/.*\sUUID="\([a-z0-9-]*\)"\s.*/\1/'`
+		cfg="\tlinux (lo)/isolinux/vmlinuz repo=hd:UUID=$uuid:/iso/\n"
+		cfg=$cfg"\tinitrd (lo)/isolinux/initrd.img"
+		;;
+
+	ubuntu)
+		# FIXME
+		cfg="\tlinux (lo)/casper/vmlinuz.efi boot=casper iso-scan/filename=/iso/$fn\n"
+		cfg=$cfg"\tinitrd (lo)/casper/initrd.lz"
+		;;
+	*)
+		echo "Warning: distribution $id not supported (skipped)!"
+		continue
+		;;
+	esac
+
+	echo -e "\nmenuentry '$id $ver Install' {" >> $grub_cfg
+	echo -e "\tloopback lo /iso/$fn" >> $grub_cfg
+	echo -e $cfg >> $grub_cfg
+	echo -e "}" >> $grub_cfg
+done
+
+echo
