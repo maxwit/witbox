5499b27f666c5f4937091fbc6efec99f249df780
Conke Hu <conke.hu@maxwit.com>
2015-02-14 01:34:04 -0800

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- witdisk/part.sh
+++ witdisk/part.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ $USER != root ]; then
+if [ $USER != root ]
+then
 	echo "must run as super user!"
 	exit 1
 fi
 
-if [ $# != 3 ]; then
+if [ $# != 3 ]
+then
 	echo "usage: $0 -s <system list> <disk>"
 	echo "i.e.: $0 -s w,x,l /dev/sdb"
 	exit 1
@@ -14,46 +16,76 @@ fi
 dev=$3
 # FIXME
 disk=${dev%%[0-9]}
-if [ "$disk" != "$dev" ]; then
+if [ "$disk" != "$dev" ]
+then
 	echo "warning: $dev does NOT seem a disk, fall back to $disk"
 fi
 
-if [ ! -b $disk ]; then
+if [ ! -b $disk ]
+then
 	echo "No such device: $disk!"
 	exit 1
 fi
 
 umount ${disk}*
+#umount ${disk}* 2>/dev/null
+
+part_label=()
+part_type=()
 
 sys=$2
 sch=""
+i=0
 
 for s in ${sys//,/ }
 do
-	size=""
+	if [ "${s:1:1}" == "=" ]
+	then
+		size=${s:2}
+	else
+		size=""
+	fi
 
 	case $s in
 	w|w=*)
-		echo windows
-		size=5G
+		part_label[$i]="windows"
+		part_type[$i]="ntfs"
+		if [ "$size" == "" ]
+		then
+			size=5G
+		fi
 		;;
+
 	l|l=*)
-		echo linux
-		size=1536M
+		part_label[$i]="linux"
+		part_type[$i]="ext4"
+		if [ "$size" == "" ]
+		then
+			size=1536M
+		fi
 		;;
 	x|x=*)
-		echo OS X
-		size=1G
+		part_label[$i]="OSX"
+		part_type[$i]="vfat"
+		if [ "$size" == "" ]
+		then
+			size=9G
+		fi
 		;;
 	*)
 		echo "Invalid OS type: $s"
 		exit 1
 	esac
 
-	if [ "$size" != "" ]; then
+	echo "size = $size"
+
+	if [ "$size" != "" ]
+	then
 		size="+$size"
 	fi
 	sch=$sch"n\n\n\n\n${size}\n"
+
+	((i++))
 done
 
 #sch=$sch"w\n"
@@ -63,15 +95,33 @@ echo "creating partitions ..."
 parted -s $disk mktable msdos
 echo -e $sch | fdisk $disk
 
-exit 0
-
 #kpartx -a $disk
 
-mkfs.ntfs ${disk}1
-ntfslabel ${disk}1 WINDOWS
+for ((j=0; j<i; j++))
+do
+	type=${part_type[$j]}
+	part=${disk}$((j+1))
+	label=${part_label[$j]}
+	echo "formating $part with $type ..."
 
-mkfs.vfat -F32 ${disk}2
-dosfslabel ${disk}2 OSX
+	case $type in
+	vfat)
+		mkfs.vfat -F32 $part
+		dosfslabel $part $label
+		;;
+	ntfs)
+		mkfs.ntfs $part
+		ntfslabel $part $label
+		;;
+	ext[234])
+		mkfs.$type -F $part
+		e2label $part $label
+		;;
+	*)
+		echo "Invalid file system: $type!"
+		exit 1
+		;;
+	esac
 
-mkfs.ext4 -F ${disk}3
-e2label ${disk}3 linux
+	echo
+done
