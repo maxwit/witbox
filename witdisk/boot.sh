ca311f7844253ea1a544c7a234bd06725b9b53aa
conke.hu@maxwit.com <Conke Hu>
2015-03-08 12:21:34 +0800

    update
    Signed-off-by: conke.hu@maxwit.com <Conke Hu>

--- witdisk/boot.sh
+++ witdisk/boot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-if [ $# != 1 ]
-then
+if [ $# != 1 ]; then
 	echo "usage: $0 <mount point>"
 	exit 1
 fi
@@ -12,15 +11,13 @@ part=""
 while read mnt
 do
 	mnt=($mnt)
-	if [ ${boot} == ${mnt[1]} ]
-	then
+	if [ ${boot} == ${mnt[1]} ]; then
 		part=${mnt[0]}
 		break
 	fi
 done < /proc/mounts
 
-if [ "$part" == "" ]
-then
+if [ "$part" == "" ]; then
 	echo "No such mount point found! ($boot)"
 	exit 1
 fi
@@ -55,19 +52,16 @@ done
 echo "installing grub to $boot for $disk ..."
 
 grub_cmd=`which grub2-install`
-if [ -z $grub_cmd ]
-then
+if [ -z $grub_cmd ]; then
 	grub_cmd="grub-install"
 fi
 
 $grub_cmd --boot-directory=$boot $disk
 
 ############# generate grub.cfg #############
-if [ -d "$boot/grub2" ]
-then
+if [ -d "$boot/grub2" ]; then
 	grub_cfg="$boot/grub2/grub.cfg"
-elif [ -d "$boot/grub" ]
-then
+elif [ -d "$boot/grub" ]; then
 	grub_cfg="$boot/grub/grub.cfg"
 else
 	echo "The grub directory does not exist!"
@@ -84,17 +78,17 @@ do
 	dist=(${fn//-/ })
 
 	id=${dist[0]}
-	ver=${dist[1]}
+	ver=${dist[1]} # FIXME
 
 	echo "generating menuentry for $id-$ver ..."
 	case $id in
-	CentOS|RHEL|Fedora|OLinux)
+	RHEL | CentOS | OLinux | Fedora)
 		uuid=`blkid $part | sed 's/.*\sUUID="\([a-z0-9-]*\)"\s.*/\1/'`
 		linux="linux (lo)/isolinux/vmlinuz repo=hd:UUID=$uuid:/iso/"
 		initrd="initrd (lo)/isolinux/initrd.img"
 		;;
 
-	ubuntu)
+	ubuntu | deiban)
 		linux="linux (lo)/casper/vmlinuz.efi boot=casper iso-scan/filename=/iso/$fn"
 		initrd="initrd (lo)/casper/initrd.lz"
 		;;
