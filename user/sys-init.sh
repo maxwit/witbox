feea315d896ba0a39b581e38adebb1c27f6e5812
Conke Hu <conke.hu@maxwit.com>
2015-04-26 21:24:26 -0700

    update sys-init
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- user/sys-init.sh
+++ user/sys-init.sh
@@ -1,49 +1,61 @@
 #!/bin/bash
 
-#sudo passwd root
-#sudo visudo 
+if [ $UID != 0 ]; then
+	echo "pls run as root!"
+	exit
+fi
 
 dist=`lsb_release -si`
 
 case "$dist" in
 Ubuntu|Debian)
-	sudo apt-get upgrade -y
-	sudo ln -svf bash /bin/sh
-	sudo update-alternatives --set editor /usr/bin/vim.basic
-	sudo apt-get install -y git gcc g++ vim
+	sed -i 's/\(^%sudo\s\+.*\s\)ALL/\1NOPASSWD:ALL/' /etc/sudoers
+	apt-get upgrade -y
+	apt-get install -y git gcc g++ vim emacs
 	# FIXME with dpkg-reconfigure
+	ln -svf bash /bin/sh
+	update-alternatives --set editor /usr/bin/vim.basic
 	;;
 *) # FIXME
-	sudo yum install -y http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-	sudo yum install -y git gcc gcc-c++ vim
+	yum install -y http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+	yum install -y git gcc gcc-c++ vim emacs
 	# FIXME
-	sudo cp -v /usr/bin/vim /bin/vi
+	cp -v /usr/bin/vim /bin/vi
 	;;
 esac
+
+groupadd -g 3000 devel
+groupadd -g 5000 maxwit
+usermod -g devel -a -G maxwit $USER
+groupdel $USER
+
+# groups=$(groups $SUDO_USER)
+# groups=${groups/$USER /}
+# groups=${groups// /,}
+# useradd -g devel -G maxwit,$groups -c "Ting Yang" ting
+# passwd ting 
 
 for part in `ls /dev/sda[0-9]*`
 do
 	index=${part#/dev/sda}
-	sudo mkdir -vp /mnt/$index
+	mkdir -vp /mnt/$index
 done
 
-sudo mount LABEL=maxwit /mnt && {
-	sudo umount /mnt
-	sudo mkdir -p /maxwit
-	grep "LABEL=maxwit" /etc/fstab || sudo sed -i '$a\LABEL=maxwit /maxwit ext4 defaults 0 0' /etc/fstab
-	sudo mount /maxwit
+mount LABEL=maxwit /mnt && {
+	umount /mnt
+	WITPATH="/mnt/maxwit"
+	mkdir -vp $WITPATH
+	# FIXME
+	grep "LABEL=maxwit" /etc/fstab || sed -i '$a\LABEL=maxwit '"$WITPATH"' ext4 defaults 0 0' /etc/fstab
+	mount $WITPATH && umount $WITPATH
+} || {
+	WITPATH="/opt/maxwit"
+	mkdir -vp $WITPATH
+	# TODO: init sync?
 }
 
-sudo groupadd -g 3000 devel
-sudo groupadd -g 5000 maxwit
-sudo usermod -g devel -a -G maxwit $USER
-sudo groupdel $USER
-
-groups=`groups`
-groups=${groups/$USER /}
-groups=${groups// /,}
-sudo useradd -g devel -G maxwit,$groups -c "Ting Yang" ting
-sudo passwd ting 
+echo "WITPATH=$WITPATH"
+chown $SUDO_USER.devel -R /opt $WITPATH
 
 echo
 echo "****************************************"
