7a0d04dac8b55fdc85c54e422cbf802ddfdb526a
Conke Hu <conke.hu@maxwit.com>
2015-06-02 20:11:56 +0800

    add createpartition
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- boot-disk/part.sh
+++ boot-disk/part.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ $USER != root ]
-then
+if [ $USER != root ]; then
 	echo "must run as super user!"
 	exit 1
 fi
 
-if [ $# != 3 ]
-then
+if [ $# != 3 ]; then
 	echo "usage: $0 -s <system list> <disk>"
 	echo "i.e.: $0 -s w,x,l /dev/sdb"
 	exit 1
@@ -16,13 +14,11 @@ fi
 dev=$3
 # FIXME
 disk=${dev%%[0-9]}
-if [ "$disk" != "$dev" ]
-then
+if [ "$disk" != "$dev" ]; then
 	echo "warning: $dev does NOT seem a disk, fall back to $disk"
 fi
 
-if [ ! -b $disk ]
-then
+if [ ! -b $disk ]; then
 	echo "No such device: $disk!"
 	exit 1
 fi
@@ -38,8 +34,7 @@ i=0
 
 for s in ${sys//,/ }
 do
-	if [ "${s:1:1}" == "=" ]
-	then
+	if [ "${s:1:1}" == "=" ]; then
 		size=${s:2}
 	else
 		size=""
@@ -49,8 +44,7 @@ do
 	w|w=*)
 		part_label[$i]="windows"
 		part_type[$i]="ntfs"
-		if [ "$size" == "" ]
-		then
+		if [ "$size" == "" ]; then
 			size=5G
 		fi
 		;;
@@ -58,16 +52,14 @@ do
 	l|l=*)
 		part_label[$i]="linux"
 		part_type[$i]="ext4"
-		if [ "$size" == "" ]
-		then
+		if [ "$size" == "" ]; then
 			size=6G
 		fi
 		;;
 	x|x=*)
 		part_label[$i]="OSX"
 		part_type[$i]="vfat"
-		if [ "$size" == "" ]
-		then
+		if [ "$size" == "" ]; then
 			size=8G
 		fi
 		;;
@@ -78,8 +70,7 @@ do
 
 	echo "size = $size"
 
-	if [ "$size" != "" ]
-	then
+	if [ "$size" != "" ]; then
 		size="+$size"
 	fi
 	sch=$sch"n\n\n\n\n${size}\n"
