52c64cb1b1522af67fce4f78b182015b028a6c01
Conke Hu <conke.hu@maxwit.com>
2015-07-28 12:34:57 +0800

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- user/sys-init.pl
+++ user/sys-init.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl -w
 
-use strict;
-
 die 'pls run as root ' if ($> != 0);
 
+use strict;
+use Linux::Distribution qw(distribution_name distribution_version);
+
+my $linux = Linux::Distribution->new;
+die 'known Linux' if (not $linux);
+
+print $linux->distribution_version."\n";
