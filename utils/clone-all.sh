755a71814f8900ff33cc6487a69dc2d10fbcf19f
Conke Hu <conke.hu@maxwit.com>
2015-12-13 06:20:06 -0800

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- utils/clone-all.sh
+++ utils/clone-all.sh
@@ -2,10 +2,10 @@
 
 if [ ! -d $dir ]
 then
-	mkdir -p  /$HOME/project
+	mkdir -p $HOME/project
 fi
 
-cd /$HOME/project
+cd $HOME/project
 
 projectlist=(android/witmail book/books book/java book/dba book/jee dba/deploy hadoop/hadoop iot/toolchain iot/mini-lablin iot/qemu-omap iot/fbv iot/withttp jee/mybatis-demo jee/app-demo jee/witweb-sh jee/task jee/withttp jee/deploy jee/jsp-demo jee/witweb p2p/p2p-rwx pub/book pub/jse pub/demo sys/withost sys/witbox sys/task web/e-lib)
 
