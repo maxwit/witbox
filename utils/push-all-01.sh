b7d2653f0f26c425ee1ee526ea8122b89dbc5231
Conke Hu <conke.hu@maxwit.com>
2015-12-13 07:17:02 -0800

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- utils/push-all-01.sh
+++ utils/push-all-01.sh
@@ -7,19 +7,15 @@ fi
 
 cd $HOME/project
 
-projectlist=(android/witmail book/books book/java book/dba book/jee dba/deploy hadoop/witdh iot/toolchain iot/mini-lablin iot/qemu-omap iot/fbv iot/withttp jee/mybatis-demo jee/app-demo jee/witweb-sh jee/task jee/withttp jee/deploy jee/jsp-demo jee/witweb pub/book pub/jse pub/demo sys/withost sys/witbox web/e-lib)
+projectlist=(android/witmail book/books book/java book/dba book/jee dba/deploy hadoop/witdh iot/toolchain iot/mini-lablin iot/qemu-omap iot/fbv iot/withttp jee/mybatis-demo jee/app-demo jee/witweb-sh jee/task jee/withttp jee/deploy jee/jsp-demo pub/book pub/jse pub/demo web/e-lib)
 
 for item in ${projectlist[@]};
 do
 	cd $HOME/project/$item
-	branchlist=(`git branch -r`)
+	branchlist=`git branch -r | sed '/HEAD/d'`
 
-	for branch in ${branchlist[@]}
+	for branch in $branchlist
 	do
-		if echo $branch | egrep -q HEAD
-		then
-			continue
-		fi
 		realbranch=${branch#origin/}
 		git checkout $realbranch
 		git push -u git@git.debug.live:$item $realbranch
