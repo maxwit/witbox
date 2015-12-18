702867d141c92fced0f2ab0305ae553422328e5b
Conke Hu <conke.hu@maxwit.com>
2015-12-18 21:54:52 +0800

    update
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- utils/clone-all.sh
+++ utils/clone-all.sh
@@ -7,7 +7,7 @@ fi
 
 cd $HOME/project
 
-projectlist=(android/witmail book/books book/java book/dba book/jee dba/deploy hadoop/hadoop iot/toolchain iot/mini-lablin iot/qemu-omap iot/fbv iot/withttp jee/mybatis-demo jee/app-demo jee/witweb-sh jee/task jee/withttp jee/deploy jee/jsp-demo jee/witweb p2p/p2p-rwx pub/book pub/jse pub/demo sys/withost sys/witbox sys/task web/e-lib)
+projectlist=(android/witmail book/books book/java book/dba book/jee dba/deploy hadoop/witdh iot/toolchain iot/mini-lablin iot/qemu-omap iot/fbv iot/withttp jee/mybatis-demo jee/app-demo jee/witweb-sh jee/task jee/withttp jee/deploy jee/jsp-demo jee/witweb p2p/p2p-rwx pub/book pub/jse pub/demo sys/withost sys/witbox sys/task web/e-lib)
 
 for item in ${projectlist[@]};
 do
