bd2209d2cfe68e7b63c7a00aaae0813a71e66141
Conke Hu <conke.hu@gmail.com>
2017-04-28 22:21:59 +0800

    update post-install.sh
    Signed-off-by: Conke Hu <conke.hu@gmail.com>

--- install/post-install.sh
+++ install/post-install.sh
@@ -2,27 +2,37 @@
 
 os=`uname -s`
 
+# init:
+# 1. read os version
+# 2. get installer ready
+
 case $os in
 	Linux )
+		declare -A check
+
 		if [ -e /etc/os-release ]; then
 			. /etc/os-release
-			dist_id=$ID
+			os_dist=$ID
 			version=$VERSION_ID
 		elif [ -e /etc/redhat-release ]; then
 			dist=(`cat /etc/redhat-release | head -n 1`)
-			dist_id=`echo ${dist[0]} | tr A-Z a-z`
+			os_dist=`echo ${dist[0]} | tr A-Z a-z`
 			version=${dist[2]%%.*}
 		else
 			echo -e "Fail to detect the distribution name!\n"
 			exit 1
 		fi
 
-		case $dist_id in
+		case $os_dist in
 			ubuntu|debian)
-				installer="sudo apt-get install -y"
+				which apt > /dev/null || alias apt=apt-get
+				installer="sudo apt install -y"
 				;;
-			redhat|centos|fedora|ol)
-				installer="sudo yum install -y"
+			redhat|centos|fedora )
+				which dnf > /dev/null || {
+					sudo yum install -y yum-utils || alias dnf=yum
+				}
+				installer="sudo dnf install -y"
 				;;
 			*)
 				echo "Linux distribution '$ID' not supported!"
@@ -46,22 +56,179 @@ case $os in
 		;;
 esac
 
-function install() {
-	for pkg in $@; do
-		loc=`which $pkg`
-		if [[ -z "$loc" ]]; then
-			for (( i = 0; i < 5; i++ )); do
-				$installer $pkg && break
-			done
+# log="$HOME/witbox-post-install.log"
+#
+# echo > $log
+#
+# function pkg_install() {
+# 	for pkg in $@; do
+# 		if [[ -z "${exe[$pkg]}" ]]; then
+# 			exe=$pkg
+# 		else
+# 			exe=${check[$pkg]}
+# 		fi
+#
+# 		loc=`which $exe`
+#
+# 		if [[ ! -e "$loc" ]]; then
+# 			for (( i = 0; i < 5; i++ )); do
+# 				echo $installer $pkg && break
+# 				sleep 1
+# 			done
+# 			if [[ $i -eq 5 ]]; then
+# 				echo $pkg >> $log
+# 			fi
+# 		fi
+# 	done
+# }
+
+### base and common ###
+
+pkg_list=('git' 'subversion')
+
+### Build tools ###
+
+# C/C++
+
+case $os in
+	Linux )
+		pkg_list+=(gcc)
+		case $os_dist in
+			redhat|centos|fedora )
+				pkg=gcc-c++
+				pkg_list+=($pkg)
+				check[$pkg]=g++
+				;;
+			* )
+				pkg_list+=(g++)
+				;;
+		esac
+		;;
+esac
+
+pkg_list+=(cmake)
+
+# TODO: add build tools
+
+# C#
+
+# Go
+
+# Java and Groovy
+
+# JavaScript
+
+# Perl
+
+# PHP
+
+case $os in
+	Linux )
+		case $os_dist in
+			redhat|centos )
+			 	echo FIXME: use remi/scl instead
+				;;
+			* )
+				pkg_list+=(php)
+				pkg_list+=(composer)
+				;;
+		esac
+		;;
+	Darwin )
+		pkg_list+=(homebrew/php/composer)
+esac
+
+# Python
+
+pkg_list+=(anaconda3)
+
+# Ruby
+
+# Rust
+
+# Scala
+
+# Swift
+
+# TypeScript
+
+
+### Editors: Atom/Code/Sublime/VIM ###
+
+case $os in
+	Linux )
+		case $os_dist in
+			redhat|centos|fedora )
+				# Code
+				sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+				sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+				# VIM
+				pkg_list+=(vim-enhanced)
+				check[vim-enhanced]=vim # or vimdiff
+			;;
+			ubuntu|debian )
+				# Code
+				curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+				sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+				sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+				# VIM
+				pkg_list+=(vim-gtk3) # FIXME for old ubuntu/debian
+				check[vim-gtk3]=vim.gtk3
+			;;
+		esac
+		pkg_list+=(code)
+		# git_list[atom]='https://github.com/atom/atom.git'
+		;;
+	Darwin )
+		pkg_list+=(atom visual-studio-code sublime-text)
+		check[visual-studio-code]=code
+		check[sublime-text]=subl
+		;;
+esac
+
+# update repo
+
+case $os in
+	Linux )
+		case "$os_dist" in
+			ubuntu|debian )
+				sudo apt update -y
+				sudo apt upgrade -y
+				;;
+			redhat|centos|fedora )
+				sudo dnf install -y epel-release
+				sudo dnf update -y
+				;;
+		esac
+		;;
+	Darwin )
+		echo "TODO: brew update?"
+		;;
+esac
+
+# check packages to be installed
+
+if [[ $os == Darwin ]]; then
+	cask_list=()
+	for pkg in ${pkg_list[@]}; do
+		if [[ $os == Darwin ]]; then
+			result=`brew cask search $pkg`
+			if [[ "${result:0:15}" == '==> Exact match' ]]; then
+				pkg_list=(${pkg_list[@]/$pkg})
+				cask_list+=($pkg)
+			fi
 		fi
 	done
-}
 
-install git
+	echo brew install ${pkg_list[@]}
+	echo brew cask install ${cask_list[@]}
+fi
+
+# now ready for install packages
+
+# pkg_install ${pkg_list[@]}
 
 exit 0
-
-install git
 
 case $os in
 	Linux )
@@ -69,15 +236,15 @@ case $os in
 		fullname=${fullname/,*}
 		;;
 	Darwin )
-		fullname="Conke Hu"
+		fullname=$USER # FIXME
 		;;
 esac
 
-account=${fullname// /.}
-account=$(echo $account | tr A-Z a-z)
+username=${fullname// /.}
+username=$(echo $username | tr A-Z a-z)
 
 git config --global user.name "$fullname"
-git config --global user.email $account@gmail.com
+git config --global user.email $username@gmail.com
 git config --global color.ui auto
 git config --global push.default simple
 # git config --global sendemail.smtpserver /usr/bin/msmtp
