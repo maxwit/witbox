f901f055df1745ddb531080dcd7e703be49f1ad5
Conke Hu <conke.hu@gmail.com>
2017-04-29 13:55:12 +0800

    update post-install.sh
    Signed-off-by: Conke Hu <conke.hu@gmail.com>

--- install/post-install.sh
+++ install/post-install.sh
@@ -6,12 +6,11 @@ fi
 
 declare -a pkg_list
 
+group='undefined'
+
 os=`uname -s`
 
-# init:
-# 1. read os version
-# 2. get installer ready
-
+# get OS version
 case $os in
 	Linux )
 		if [ -e /etc/os-release ]; then
@@ -23,106 +22,164 @@ case $os in
 			os_dist=`echo ${dist[0]} | tr A-Z a-z`
 			version=${dist[2]%%.*}
 		else
-			echo -e "Fail to detect the distribution name!\n"
+			echo -e "Linux distribution not supported yet!\n"
 			exit 1
 		fi
-
-		case $os_dist in
-			ubuntu|debian )
-				which apt > /dev/null 2>&1 && pm='sudo apt' || pm='sudo apt-get'
-				installer="$pm install -y"
-				;;
-			redhat|centos|fedora )
-				if [[ $os_dist == fedora ]]; then
-					# need epel?
-					pm='sudo dnf'
-				else
-					if [[ $version -ge 7 ]]; then
-						which dnf > /dev/null 2>&1 || sudo yum install -y yum-utils || {
-							echo 'fail to install yum-utils!'
-							exit 1
-						}
-						pm='sudo dnf'
-					else
-						pm='sudo yum'
-					fi
-					if [[ ! -e /etc/yum.repos.d/ius.repo ]]; then
-						curl https://setup.ius.io/ | sudo bash
-					fi
-					# if [[ ! -e /etc/yum.repos.d/remi.repo ]]; then
-					# 	yum install -y http://rpms.famillecollet.com/enterprise/remi-release-${version}.rpm
-					# fi
-				fi
-				installer="$pm install -y"
-				;;
-			*)
-				echo "Linux distribution '$ID' not supported!"
-				exit 1
-		esac
-
 		;;
 
 	Darwin )
+		os_dist=macOS
+		# TODO: versions
+		;;
+
+	* )
+		echo -e "OS '$os' not supported yet!\n"
+		exit 1
+esac
+
+echo -e "### Setup for $os_dist ###\n"
+
+# get installer ready
+case $os_dist in
+	ubuntu|debian )
+		which apt > /dev/null 2>&1 && pm='sudo apt' || pm='sudo apt-get'
+		installer="$pm install -y"
+		$pm update -y
+		$pm upgrade -y
+		;;
+
+	redhat|centos|fedora )
+		if [[ $os_dist == fedora ]]; then
+			# need epel?
+			pm='sudo dnf'
+		else
+			if [[ $version -ge 7 ]]; then
+				which dnf > /dev/null 2>&1 || sudo yum install -y yum-utils || {
+					echo 'fail to install yum-utils!'
+					exit 1
+				}
+				pm='sudo dnf --allowerasing'
+			else
+				pm='sudo yum'
+			fi
+
+			if [[ ! -e /etc/yum.repos.d/ius.repo ]]; then
+				curl https://setup.ius.io/ | sudo bash
+			fi
+			# if [[ ! -e /etc/yum.repos.d/remi.repo ]]; then
+			# 	yum install -y http://rpms.famillecollet.com/enterprise/remi-release-${version}.rpm
+			# fi
+			# and SCL ?
+		fi
+		installer="$pm install -y"
+		$pm update -y
+		;;
+
+	macOS )
 		which brew > /dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 		which brew > /dev/null 2>&1 || {
 			echo "fail to install HomeBrew, pls try again!"
 			exit 1
 		}
 		installer="brew install"
+		# TODO: update/upgrade ?
 		;;
 
 	*)
-		echo "'$os' not supported!"
+		echo "'$os_dist' not supported!"
 		exit 1
 		;;
 esac
 
-# log="$HOME/witbox-post-install.log"
-#
-# echo > $log
-#
-# function pkg_install() {
-# 	for pkg in $@; do
-# 		if [[ -z "${exe[$pkg]}" ]]; then
-# 			exe=$pkg
-# 		else
-# 			exe=${check[$pkg]}
-# 		fi
-#
-# 		loc=`which $exe`
-#
-# 		if [[ ! -e "$loc" ]]; then
-# 			for (( i = 0; i < 5; i++ )); do
-# 				echo $installer $pkg && break
-# 				sleep 1
-# 			done
-# 			if [[ $i -eq 5 ]]; then
-# 				echo $pkg >> $log
-# 			fi
-# 		fi
-# 	done
-# }
+log="$HOME/witbox-post-install.log"
+echo > $log
 
-### base and common ###
+function install_pkgs() {
+	echo "[$group]"
 
-case $os in
-	Linux )
-		case $os_dist in
-			redhat|centos )
-				pkg_list+=(git2u)
-				;;
-		* )
-				pkg_list+=(git)
-				;;
-		esac
+	count=${#pkg_list[@]}
+	echo -n "$count packages to be installed: "
+	if [[ $count -gt 0 ]]; then
+		echo "${pkg_list[@]}" | sed 's/ /, /g'
+	else
+		echo "(skipped)"
+		return
+	fi
+
+	# for pkg in $@; do
+	# 	if [[ -z "${exe[$pkg]}" ]]; then
+	# 		exe=$pkg
+	# 	else
+	# 		exe=${check[$pkg]}
+	# 	fi
+	#
+	# 	loc=`which $exe`
+	# done
+
+	for pkg in ${pkg_list[@]}; do
+		if [[ $os == macOS ]]; then
+			result=`brew cask search $pkg`
+			if [[ "${result:0:15}" == '==> Exact match' ]]; then # which one is better?
+				pkg="Caskroom/cask/$pkg"
+			# 	installer="brew cask install"
+			# else
+			# 	installer="brew install"
+			fi
+		fi
+
+		echo "Installing $pkg ..."
+		for (( i = 0; i < 3; i++ )); do
+			$installer $pkg && break
+		done
+		if [[ $i -eq 3 ]]; then
+			echo "[F] $pkg" >> $log
+		fi
+	done
+}
+
+group="SCM"
+pkg_list=()
+
+case "$os_dist" in
+	macOS )
+		;;
+	redhat|centos )
+		pkg_list+=(git2u)
+		pkg_list+=(subversion)
+		;;
+	* )
+		pkg_list+=(git)
+		pkg_list+=(subversion)
 		;;
 esac
 
-pkg_list+=(subversion)
+install_pkgs
 
-### Build tools ###
+if [[ ! -e ~/.gitconfig ]]; then
+	case $os in
+		Linux )
+			fullname=$(awk -F : -v user=$USER '$1==user {print $5}' /etc/passwd)
+			fullname=${fullname/,*}
+			;;
+		macOS )
+			fullname=$USER # FIXME
+			;;
+	esac
 
-# C/C++
+	username=${fullname// /.}
+	username=$(echo $username | tr A-Z a-z)
+
+	git config --global user.name "$fullname"
+	git config --global user.email $username@gmail.com
+	git config --global color.ui auto
+	git config --global push.default simple
+	# git config --global sendemail.smtpserver /usr/bin/msmtp
+	git config --global merge.ours.driver true
+	git config --list
+fi
+
+group="C/C++"
+pkg_list=()
 
 case $os in
 	Linux )
@@ -131,7 +188,7 @@ case $os in
 			redhat|centos|fedora )
 				pkg=gcc-c++
 				pkg_list+=($pkg)
-				check[$pkg]=g++
+				# check[$pkg]=g++
 				;;
 			* )
 				pkg_list+=(g++)
@@ -142,135 +199,151 @@ esac
 
 pkg_list+=(cmake)
 
-# TODO: add build tools
+# TODO: conan and clang
 
-# C#
+install_pkgs
 
-# Go
+group="C#"
 
-# Java and Groovy
+group="Go"
 
-# JavaScript
+group="Java and Groovy"
 
-# Perl
+group="JavaScript"
 
-# PHP
+group="Perl"
 
-case $os in
-	Linux )
-		case $os_dist in
-			redhat|centos )
-				# 	echo FIXME: use remi/scl instead
-				pkg_list+=(php56u)
-				;;
-			* )
-				pkg_list+=(php)
-				;;
-		esac
+group="PHP"
+pkg_list=()
+
+case "$os_dist" in
+	redhat|centos )
+		# use remi/scl instead?
+		pkg_list+=(php56u)
 		;;
-	Darwin )
-		pkg_list+=(homebrew/php/composer)
+
+	* )
+		pkg_list+=(php)
+		;;
 esac
 
 # TODO: install composer
 
-# Python
+install_pkgs
+
+group="Python"
+pkg_list=()
 
 case $os in
-	Darwin )
+	macOS )
 		pkg_list+=(anaconda3)
 		;;
 esac
 
-# Ruby
+install_pkgs
 
-# Rust
+group="Ruby"
 
-# Scala
+group="Rust"
 
-# Swift
+group="Scala"
 
-# TypeScript
+group="Swift"
 
+group="TypeScript"
 
-### Editors: Atom/Code/Sublime/VIM ###
+group="VIM/Emacs"
+pkg_list=()
 
-case $os in
-	Linux )
-		case $os_dist in
-			redhat|centos|fedora )
-				# Code
-				if [ $os_dist == fedora -o $version -ge 7 ]; then
-					sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-					sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-					pkg_list+=(code)
-				fi
-				# VIM
-				pkg_list+=(vim-enhanced)
-				check[vim-enhanced]=vim # or vimdiff
-			;;
-			ubuntu|debian )
-				# Code
-				curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-				sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-				sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-				pkg_list+=(code)
-				# VIM
-				pkg_list+=(vim-gtk3) # FIXME for old ubuntu/debian
-				check[vim-gtk3]=vim.gtk3
-			;;
-		esac
-		# git_list[atom]='https://github.com/atom/atom.git'
+case "$os_dist" in
+	macOS )
 		;;
-	Darwin )
+	redhat|centos|fedora )
+		pkg_list+=(vim-enhanced)
+		# check[vim-enhanced]=vim # or vimdiff
+		;;
+	ubuntu|debian )
+		pkg_list+=(vim-gtk3) # FIXME for old ubuntu/debian
+		# check[vim-gtk3]=vim.gtk3
+		;;
+	* )
+		pkg_list+=(vim)
+		;;
+esac
+
+install_pkgs
+
+[[ ! -e ~/.vimrc ]] &&	cat > ~/.vimrc << __EOF__
+set nu
+set ts=4
+set hlsearch
+let c_space_errors=1
+let java_space_errors=1
+__EOF__
+
+# spacemacs
+
+# cat > ~/.emacs << EOF
+# (global-linum-mode t)
+# EOF
+
+# Compilers and build tools #
+
+group='Atom/Code/Sublime'
+pkg_list=()
+
+case $os_dist in
+	macOS )
 		pkg_list+=(atom visual-studio-code sublime-text)
-		check[visual-studio-code]=code
-		check[sublime-text]=subl
+		# check[visual-studio-code]=code
+		# check[sublime-text]=subl
 		;;
-esac
 
-# update repo
-
-case $os in
-	Linux )
-		case "$os_dist" in
-			ubuntu|debian )
-				$pm update -y
-				$pm upgrade -y
-				;;
-			redhat|centos|fedora )
-				$pm update -y
-				;;
-		esac
-		;;
-	Darwin )
-		echo "TODO: brew update?"
-		;;
-esac
-
-# check packages to be installed
-
-if [[ $os == Darwin ]]; then
-	cask_list=()
-	for pkg in ${pkg_list[@]}; do
-		if [[ $os == Darwin ]]; then
-			result=`brew cask search $pkg`
-			if [[ "${result:0:15}" == '==> Exact match' ]]; then
-				pkg_list=(${pkg_list[@]/$pkg})
-				cask_list+=($pkg)
-			fi
+	redhat|centos|fedora )
+		# VS Code
+		if [ $os_dist == fedora -o $version -ge 7 ] && [ ! -e /etc/yum.repos.d/vscode.repo ]; then
+			sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+			sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 		fi
-	done
+		pkg_list+=(code)
+	;;
 
-	echo brew install ${pkg_list[@]}
-	echo brew cask install ${cask_list[@]}
-fi
+	ubuntu|debian )
+		# VS Code
+		if [[ ! -e /etc/apt/sources.list.d/vscode.list ]]; then
+			curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+			sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+			sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+			sudo $pm update -y
+		fi
+		pkg_list+=(code)
+	;;
+esac
 
-# now ready for install packages
-$installer ${pkg_list[@]}
+# git_list[atom]='https://github.com/atom/atom.git'
 
-# pkg_install ${pkg_list[@]}
+install_pkgs
 
+# # update repo
+#
+# case $os in
+# 	Linux )
+# 		case "$os_dist" in
+# 			ubuntu|debian )
+# 				$pm update -y
+# 				$pm upgrade -y
+# 				;;
+# 			redhat|centos|fedora )
+# 				$pm update -y
+# 				;;
+# 		esac
+# 		;;
+# 	macOS )
+# 		echo "TODO: brew update?"
+# 		;;
+# esac
+
+exit 0
 
 test -e /etc/gdm/custom.conf && {
 	temp=`mktemp`
@@ -293,37 +366,6 @@ __EOF__
 
 	sudo cp -v $temp /etc/gdm/custom.conf
 }
-
-cat > ~/.vimrc << __EOF__
-set nu
-set ts=4
-set hlsearch
-let c_space_errors=1
-let java_space_errors=1
-__EOF__
-
-case $os in
-	Linux )
-		fullname=$(awk -F : -v user=$USER '$1==user {print $5}' /etc/passwd)
-		fullname=${fullname/,*}
-		;;
-	Darwin )
-		fullname=$USER # FIXME
-		;;
-esac
-
-username=${fullname// /.}
-username=$(echo $username | tr A-Z a-z)
-
-git config --global user.name "$fullname"
-git config --global user.email $username@gmail.com
-git config --global color.ui auto
-git config --global push.default simple
-# git config --global sendemail.smtpserver /usr/bin/msmtp
-git config --global merge.ours.driver true
-git config --list
-
-exit 0
 
 function instal_vm_tools() {
 	$installer open-vm-tools
@@ -380,13 +422,3 @@ if [[ $os == Linux ]]; then
 		# virtualbox
 	esac
 fi
-
-exit 0
-
-# spacemacs
-
-cat > ~/.emacs << EOF
-(global-linum-mode t)
-EOF
-
-echo
