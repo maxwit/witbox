e50d39f6ceece25da86bad2aefff29245b645345
Conke Hu <conke.hu@gmail.com>
2018-03-13 01:47:15 +0800

    update run.sh: support efk, helm and ingress!
    Signed-off-by: Conke Hu <conke.hu@gmail.com>

--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
-# Copy ``inventory/sample`` as ``inventory/mycluster``
+mkdir -p $HOME/projects && cd $HOME/projects
+
+if [ ! -d kubespray ]; then
+	git clone https://github.com/kubernetes-incubator/kubespray.git
+fi
+
+cd kubespray
+
 cp -rfp inventory/sample inventory/mycluster
 
-# Update Ansible inventory file with inventory builder
-declare -a IPS=(192.168.18.21 192.168.18.22 192.168.18.23)
+# FIXME
+declare -a IPS=(192.168.18.11 192.168.18.12 192.168.18.13)
 CONFIG_FILE=inventory/mycluster/hosts.ini python3 contrib/inventory_builder/inventory.py ${IPS[@]} || exit 1
 
-# Review and change parameters under ``inventory/mycluster/group_vars``
 # cat inventory/mycluster/group_vars/all.yml
-# cat inventory/mycluster/group_vars/k8s-cluster.yml
 
-# Deploy Kubespray with Ansible Playbook
-ansible-playbook -i inventory/mycluster/hosts.ini cluster.yml
+sed -i.orig -e 's/.*\(ingress_nginx_enabled:\).*/\1 true/' \
+	-e 's/.*\(efk_enabled:\).*/\1 true/' \
+	-e 's/.*\(helm_enabled:\).*/\1 true/' \
+	inventory/mycluster/group_vars/k8s-cluster.yml
+
+ansible-playbook -u root -i inventory/mycluster/hosts.ini cluster.yml
