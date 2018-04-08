7f4e241061c9409077d0872c064625bca4ebca36
Conke Hu <conke.hu@gmail.com>
2018-04-09 02:21:36 +0800

    update aio.sh
    Signed-off-by: Conke Hu <conke.hu@gmail.com>

--- openstack/aio.sh
+++ openstack/aio.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-# if [ -e /etc/redhat-release ]; then
-#     yum install -y bzip2-devel libaio MariaDB-client MariaDB-devel MariaDB-shared
-#     cat > /etc/yum.repos.d/mariadb.repo << __EOF__
-# [mariadb]
-# name = MariaDB
-# baseurl = http://yum.mariadb.org/10.3/centos7-amd64
-# gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
-# gpgcheck=1
-# __EOF__
-# else
-#     apt install -y build-essential libaio1 libdbd-mysql-perl libmariadbclient-dev mariadb-client
-# fi || exit 1
-
 if [ ! -d /opt/openstack-ansible ]; then
     git clone https://git.openstack.org/openstack/openstack-ansible \
         /opt/openstack-ansible || exit 1
@@ -21,23 +8,36 @@ fi
 cd /opt/openstack-ansible && \
 git checkout stable/queens || exit 1
 
-## FIXME
-#disks=(`fdisk -l | grep '^Disk /dev' | awk '{print $2}'`)
-#rootd=`awk '$2=="/" {print $1}' /proc/mounts`
+# # FIXME
+# disks=(`fdisk -l | grep '^Disk /dev' | awk '{print $2}'`)
+# rootd=`awk '$2=="/" {print $1}' /proc/mounts`
 #
-#if [ ${#disks[@]} -gt 1 ]; then
-#	for disk in ${disks[@]/:/}; do
-#		if [ ${rootd#$disk} == $rootd ]; then
-#			export BOOTSTRAP_OPTS="bootstrap_host_data_disk_device=${disk/\/dev\//}"
-#			break
-#		fi
-#	done
-#fi
+# if [ ${#disks[@]} -gt 1 ]; then
+# 	for disk in ${disks[@]/:/}; do
+# 		if [ ${rootd#$disk} == $rootd ]; then
+# 			export BOOTSTRAP_OPTS="bootstrap_host_data_disk_device=${disk/\/dev\//}"
+# 			break
+# 		fi
+# 	done
+# fi
 #
-#echo "BOOTSTRAP_OPTS = $BOOTSTRAP_OPTS"
+# echo "BOOTSTRAP_OPTS = $BOOTSTRAP_OPTS"
 
+mkdir -vp ~/.pip
+cat > ~/.pip/pip.conf << _EOF_
+[global]
+index-url = http://mirrors.aliyun.com/pypi/simple/
+[install]
+trusted-host = mirrors.aliyun.com
+_EOF_
+
+export BOOTSTRAP_OPTS="bootstrap_host_ubuntu_repo=http://mirrors.aliyun.com/ubuntu/"
 # export ANSIBLE_ROLE_FETCH_MODE='galaxy'
+
 scripts/bootstrap-ansible.sh || exit 1
+
+sed -i '/^lxc_image_cache_server_mirrors:/a \ \ - https://mirrors.tuna.tsinghua.edu.cn/lxc-images' \
+    /etc/ansible/roles/lxc_hosts/defaults/main.yml || exit 1
 
 # export SCENARIO='ceph'
 scripts/bootstrap-aio.sh || exit 1
