ca49f84d282f5d5659a18c51771216845c26dea0
Conke Hu <conke.hu@maxwit.com>
2019-07-03 23:48:09 +0800

    misc fix
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- os-install/disk/createinstallmedia.py
+++ os-install/disk/createinstallmedia.py
@@ -43,6 +43,8 @@ parser.add_option('-p', '--isopath', dest='isopath',
                   help='path to ISO image')
 parser.add_option('-m', '--volume', dest='volume',
                   help='mount point')
+# parser.add_option('-d', '--drive', dest='drive',
+#                   help='mount point')
 
 (opts, args) = parser.parse_args()
 
@@ -147,10 +149,6 @@ if pttype == 'gpt':
         print("ESP partition not found!")
         exit(1)
 
-    for _part in os.listdir('/dev'):
-        if re.match(disk + '\d+', _part): # or + '\d+p\d+'
-            subprocess.call('umount ' + _part)
-
     efi_dir = boot_dir + '/efi'
     if not os.path.exists(efi_dir):
         os.mkdir(efi_dir)
@@ -198,7 +196,7 @@ for iso in iso_list:
         continue
 
     menuentry = [
-        "menuentry 'Install " + label + " {",
+        "menuentry 'Install " + label + "' {",
         "    set root='hd0,{}'".format(index),
         "    loopback lo {}".format(iso_rel),
         "    linux (lo)/{}".format(linux),
